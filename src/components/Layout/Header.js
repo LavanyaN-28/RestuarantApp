789fa5ddb3b1da0c6cec7e48d3885575699e8332
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-07-03 18:02:37 +0530

    Added open and close functionality of the Cart

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React,{ Fragment } from 'react';
+
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+    return (
+    <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
+        </header>
+        <div className={classes["main-image"]}> 
+         <img src="https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='A table full of delicious food'/>
+        </div>
+    </Fragment>
+    )
+}
+
+export default Header;
